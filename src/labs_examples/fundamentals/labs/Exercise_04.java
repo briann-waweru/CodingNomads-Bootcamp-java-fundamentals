ce4be13468fd350e3bbd07c28618d59a5e82af03
proficienttechscientist <proficienttechscientist@gmail.com>
2024-03-31 12:56:04 +0300

    Exercise_04 Assignment

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,27 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        boolean isEmpty = false;
+        System.out.println("boolean isEmpty is: " + isEmpty);
+
+        char c = 'a';
+        System.out.println("char c is: " + c);
+
+        byte myBte = 1;
+        System.out.println("byte myBte is: " + myBte);
+
+        short myShort =  1000;
+        System.out.println("short myShort is: " + myShort);
+
+        long myLong = 6752426542642242L;
+        System.out.println("long myLong is: " + myLong);
+
+        float myFloat = 1.2f;
+        System.out.println("float myFloat is: " + myFloat);
+
+        double myDouble = 1.2;
+        System.out.println("double myDouble is: " + myDouble);
+
 
     }
 
