333dca11f0356f407b8cf511ad0b53a9689edd73
proficienttechscientist <proficienttechscientist@gmail.com>
2024-04-12 06:40:11 +0300

    Exercise_01.java Assignment

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -1,6 +1,7 @@
 package labs_examples.arrays.labs;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Arrays calculator
@@ -13,5 +14,29 @@ import java.util.Arrays;
  */
 
 public class Exercise_01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter 10 numbers: ");
+
+
+        //Store the numbers in an array
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        //Calculate the sum of all the numbers in the array
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+
+        //Calculate the average of all the numbers in the array
+        double average = sum / array.length;
+
+        //Print the results to the console
+        System.out.println("The sum of all the numbers in the array is: " + sum);
+        System.out.println("The average of all the numbers in the array is: " + average);
+    }
 
 }
