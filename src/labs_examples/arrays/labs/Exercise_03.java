516a413bfec7c5171ebcf18758b7d6f791bf8925
proficienttechscientist <proficienttechscientist@gmail.com>
2024-04-12 06:55:16 +0300

    Implement 5x5 2D array with multiples of 3

--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -18,6 +18,24 @@ package labs_examples.arrays.labs;
 public class Exercise_03 {
 
     public static void main(String[] args) {
-        
+        // Step 1: Declare a 5x5 2D array
+        int[][] array = new int[5][5];
+
+        // Step 2: Populate the array with multiples of 3
+        int value = 3; // Starting value
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = value;
+                value += 3; // Increment by 3 for the next element
+            }
+        }
+
+        // Step 3: Print out the array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
     }
 }
