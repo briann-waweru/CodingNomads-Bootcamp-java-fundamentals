4a3f0dd5e79de937b8ade02375aacee5795db453
proficienttechscientist <proficienttechscientist@gmail.com>
2024-04-12 06:41:25 +0300

    Exercise_02.java Assignment

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -1,5 +1,9 @@
 package labs_examples.arrays.labs;
 
+import java.util.Scanner;
+
+
+
 /**
  *  More labs_examples.arrays
  *
@@ -12,10 +16,24 @@ public class Exercise_02 {
 
     public static void main(String[] args) {
 
-        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        // Initialize and populate the array with numbers 1 to 10
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 10 - i; // This will fill the array with numbers 10 to 1
+        }
 
-        // write code here
+        // Prompt the user for a number
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a number between 1 and 10: ");
+        int numbers = scanner.nextInt();
+
+        // Find and print the index of the number in the array
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == numbers) {
 
 
+                System.out.println("Index of " + numbers + " is " + i);
+            }
+        }
     }
 }
