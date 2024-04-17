8571f20b92bf406bf035fbacf7bc7ea825fd395e
proficienttechscientist <proficienttechscientist@gmail.com>
2024-04-17 05:36:59 +0300

    Updated Exercise_04. java Assignment

--- src/labs_examples/arrays/labs/Exercise_04.java
+++ src/labs_examples/arrays/labs/Exercise_04.java
@@ -9,5 +9,26 @@ package labs_examples.arrays.labs;
  */
 
 public class Exercise_04 {
+    public static void main(String[] args) {
+        //Step 1: Create and populate a 2 D with irreular sizes
+        int [] [] irregularArray = {
+                {1,2,3},
+                {4,5},
+                {6,7,8,9},
+                {10},
+                {11,12,13,14,15}
+        };
+
+        //Step 2: Iterate and print out each element of the array using a nested "for-each" loop
+        for(int [] innerArray: irregularArray){
+            for(int element: innerArray){
+                System.out.print(element + " ");
+            }
+
+            //Move to the next line after printing each inner array
+            System.out.println();
+        }
+
+    }
 
 }
