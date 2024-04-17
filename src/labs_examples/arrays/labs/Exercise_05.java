60130c149983b8e6ac197cbb6acfae8bff5cb9cf
proficienttechscientist <proficienttechscientist@gmail.com>
2024-04-17 06:06:50 +0300

    Printing an Array in reverse order

--- src/labs_examples/arrays/labs/Exercise_05.java
+++ src/labs_examples/arrays/labs/Exercise_05.java
@@ -10,4 +10,24 @@ package labs_examples.arrays.labs;
 
 public class Exercise_05 {
 
+    public static void main(String[] args) {
+        int [] array = {1,2,3,4,5,6,7,8,9,10};
+
+        System.out.println("_____________________Printing an Array in forward order_____________________");
+
+        //Printing an array in forwardrd order
+        for(int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+        System.out.println("_____________________Printing an Array in reverse order_____________________");
+
+        //Printing an array in reverse order
+        for (int j = array.length - 1; j >= 0; j--) {
+            System.out.println(array[j]);
+        }
+    }
+
+
+
 }
