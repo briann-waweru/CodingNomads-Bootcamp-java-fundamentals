84f253e5903d0b305500ed1149852b8797bcbc3d
proficienttechscientist <proficienttechscientist@gmail.com>
2024-05-11 17:36:18 +0300

    Calculating the Volume and Surface Area of a Cylinder

--- src/labs_examples/datatypes_operators/labs/Exercise_06.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_06.java
@@ -13,6 +13,13 @@ public class Exercise_06 {
     public static void main(String[] args) {
 
         // write code here
+        double radius = 3.14;
+        double height = 5;
+        double volume = radius * radius * height * 3.14;
+        double surfaceArea = 2 * radius * radius * 3.14 + 2 * radius * height * 3.14;
+
+        System.out.println("Volume: " + volume);
+        System.out.println("Surface Area: " + surfaceArea);
 
     }
 }
