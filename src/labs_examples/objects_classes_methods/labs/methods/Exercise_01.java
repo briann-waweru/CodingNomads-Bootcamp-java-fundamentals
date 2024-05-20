933afee26b64a4a959f94546801d9a3bd48722ab
briann-waweru <proficienttechscientist@gmail.com>
2024-05-20 06:49:12 +0300

    Java methods Exercise-01

--- src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
+++ src/labs_examples/objects_classes_methods/labs/methods/Exercise_01.java
@@ -9,27 +9,45 @@ public class Exercise_01 {
 
         // please create the methods as instructed below then
         // call each of those methods from here, within the main()
+        System.out.println(multiply(5, 3));
+        System.out.println(divide(10, 2));
+        printJoke();
+        System.out.println(yearsToSeconds(5) + " seconds.");
+        System.out.println(getVarargsLength(1, 2, 3, 4, 5));// method to test your code
+
 
     }
 
 
     // 1) Create a static multiply() method below that takes two int arguments (int a, int b) and
     //    returns the result of a * b
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
 
 
     // 2) Create a static divide() method below that takes two int arguments (int a, int b) and
     //    returns the result of a / b
-
+    public static int divide(int a, int b) {
+        return a / b;
+    }
 
     // 3) Create a static void method that will print of joke of your choice to the console
-
+    public static void printJoke() {
+        System.out.println("What do you call a fish with no eyes? A fsh.");
+    }
 
     // 4) Create a static method that takes in a number in years (int years) as an argument
     //    and returns the number of seconds that number in years represents
-
+    public static int yearsToSeconds(int years) {
+        return years * 365 * 24 * 60 * 60;
+    }
 
     // 5) Create a varargs method that will return the length of the varargs array passed in
 
+    public static int getVarargsLength(int... numbers) {
+        return numbers.length;
+    }
 
 
 
