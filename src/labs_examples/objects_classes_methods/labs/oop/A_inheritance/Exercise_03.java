03688f63de11df0183b8c5e3a83d2a0aaef9135e
Brian Waweru <proficienttechscientist@gmail.com>
2024-07-23 10:27:36 +0300

    Exercise_03 -> demonstrate constructor chaining in Java inheritance hierarchy

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_03.java
@@ -3,7 +3,10 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Why does the output print in the order it does?
  *
- * You answer:
+ * Your answer: The output prints in this order because in Java, when an object of a subclass is created,
+ * the constructors of all its superclasses are called first,
+ * starting from the topmost superclass down to the subclass.
+ * Each superclass constructor is called implicitly before the constructor of the current class is executed.
  *
  */
 
