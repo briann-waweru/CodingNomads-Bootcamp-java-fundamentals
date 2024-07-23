1d8e4c8a3c70e70e39b84ae2532335f38c8f1dfa
Brian Waweru <proficienttechscientist@gmail.com>
2024-07-23 10:19:58 +0300

    Exercise_02 -> Demonstrate variable hiding in inheritance by creating classes A and B with same variable name and different values

--- src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
+++ src/labs_examples/objects_classes_methods/labs/oop/A_inheritance/Exercise_02.java
@@ -3,7 +3,7 @@ package labs_examples.objects_classes_methods.labs.oop.A_inheritance;
 /**
  * Run the class below and study the output. What prints and why?
  *
- * Your answer:
+ * Your answer:The output is `10` because the variable `i` from the superclass `A` is accessed, as variables are resolved at compile time based on the reference type, not the object type.
  *
  */
 class A {
